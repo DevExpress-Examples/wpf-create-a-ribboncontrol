83efbfaaeabe0d3be450d5bcc5d27c3b27bb2c3a
DevExpressExampleBot <fakeEmail>
2020-12-28 19:23:11 +0300

    Source auto update [skip ci]

---
+++ VB/RibbonControl_Ex/Application.xaml.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Windows
+
+Namespace RibbonControl_Ex
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+
+		Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
+
+			DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName()
+		End Sub
+	End Class
+End Namespace
